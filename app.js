24020714ec8dd80441ebf2cebe762779736664cc
Erica Williams <54540259+ewilliams12395@users.noreply.github.com>
2019-11-04 10:45:37 -0500

    initial commit - add all files

---
+++ app.js
@@ -0,0 +1,49 @@
+// jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+app.set('view engine', 'ejs');
+
+const output = [];
+
+
+app.get("/", function(req, res){
+
+res.render("home", {tip: output});
+
+});
+
+
+app.post("/", function(req, res){
+
+
+
+var billAmount = req.body.billAmount;
+var percentages = req.body.percentages;
+var numPeople = req.body.totalPeople;
+
+var tip = (billAmount * percentages) / numPeople;
+tip = Math.round(tip * 100) / 100;
+tip = tip.toFixed(2);
+
+output.push(tip);
+
+res.redirect("/");
+
+
+
+});
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000);
